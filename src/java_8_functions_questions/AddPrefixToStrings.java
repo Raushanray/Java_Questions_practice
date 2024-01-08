fb26d142db53930d9f233649ac4296dab8f88e90
Raushanray <raushankuray1998@gmail.com>
2024-01-08 18:07:50 +0530

    Adding Prefix of Stings.

--- src/java_8_functions_questions/AddPrefixToStrings.java
+++ src/java_8_functions_questions/AddPrefixToStrings.java
@@ -4,17 +4,17 @@ package java_8_functions_questions;
 Write a Java 8 program that takes a list of strings as input and uses a function
 to add a prefix to each string. Then, return the list of modified strings as a new list.
  */
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class AddPrefixToStrings {
     public static void main(String[] args) {
-        List<String> strings= Arrays.asList("apple", "banana", "orange", "grape", "kiwi");
+        List<String> strings = Arrays.asList("apple", "banana", "orange", "grape", "kiwi");
         String prefix = "fruit_";
-        Function<String , String> addPrefixFunction = str -> prefix + str;
-        List<String> modifiedStrings = strings.stream().map(addPrefixFunction).collect(Collectors.toList());
+//        Function<String , String> addPrefixFunction = str -> prefix + str;
+        List<String> modifiedStrings = strings.stream().map(str -> prefix + str).collect(Collectors.toList());
         System.out.println("modified Strings : " + modifiedStrings);
     }
 }
