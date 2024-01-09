c00a550a7b721f1e70684cfd72405c6060f613d9
Raushanray <raushankuray1998@gmail.com>
2024-01-09 19:50:33 +0530

    This problem solved using java 8 and here i divided this in multiple line.

--- src/java_8_functions_questions/collections/FetchAllObjGreaterThanGivenValue.java
+++ src/java_8_functions_questions/collections/FetchAllObjGreaterThanGivenValue.java
@@ -1,8 +1,16 @@
 package java_8_functions_questions.collections;
+/*
+If we want to process bulk objects of collection then go for stream concept.
+Way to operate on collection in java-8 is stream.
+its a special iterator class that allows processing collections of objects in a functional manner.
+
+Eg: fetch all objects from collection of list whose value is greater than 15.
+ */
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FetchAllObjGreaterThanGivenValue {
     public static void main(String[] args) {
@@ -12,7 +20,7 @@ public class FetchAllObjGreaterThanGivenValue {
         lists.add(36);
         lists.add(12);
         lists.add(14);
-        lists.add(16);
+        lists.add(46);
         //without stream
         /*List<Integer> asListFromMethod = findElement(lists);
         for (Integer i : asListFromMethod){
@@ -37,6 +45,10 @@ public class FetchAllObjGreaterThanGivenValue {
 //        List<Integer> integerList = lists.stream().filter(x -> x >= 15).collect(Collectors.toList());
 //        List<Integer> integerList = lists.stream().filter(x -> x >= 15).toList();
 //                integerList.forEach(System.out::println);
-          lists.stream().filter(x -> x >= 15).toList().forEach(System.out::println);
+//        lists.stream().filter(x -> x >= 15).toList().forEach(System.out::println);
+        Stream<Integer> openStream = lists.stream();
+        Stream<Integer> filteredStream = openStream.filter(x -> x >= 20);
+        List<Integer> newFilteredStream = filteredStream.collect(Collectors.toList());
+        newFilteredStream.stream().forEach(y -> System.out.println(y));
     }
 }
