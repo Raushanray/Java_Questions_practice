da556a21adf9f5addb63e00babf16a930e7807b6
Raushanray <raushankuray1998@gmail.com>
2024-01-15 17:22:39 +0530

    write a program to find the even number and count the all even numbers of given range. using java8.

--- src/java_8_functions_questions/EvenSumCalculateWithRange.java
+++ src/java_8_functions_questions/EvenSumCalculateWithRange.java
@@ -4,7 +4,9 @@ import java.util.stream.IntStream;
 
 public class EvenSumCalculateWithRange {
     public static void main(String[] args) {
+        long count = IntStream.rangeClosed(1, 12).filter(n -> n % 2 == 0).count();
         int sum = IntStream.rangeClosed(1, 12).filter(n -> n % 2 == 0).sum();
+        System.out.println("Count of even numbers : " + count);
         System.out.println("sum of even numbers : " + sum);
     }
 }
